c527979974e82a466eb79185673c4cc206f4a071
Pavel Osw <pavel.olage@alumnos.udg.mx>
2022-10-10 22:59:58 -0500

    06 Desestructuracion de funciones

---
+++ src/ejercicios/06-desestructuracion-funcion.ts
@@ -0,0 +1,37 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+
+interface Producto {
+    desc: string;
+    precio: number;
+}
+
+const telefono: Producto = {
+    desc: 'Redmi Note 7',
+    precio: 149
+}
+
+const tablet: Producto = {
+    desc: 'iPad Air 2',
+    precio: 349
+}
+
+function calculaIVA( productos: Producto[]): [number, number] {
+    
+    let total = 0;
+
+    productos.forEach( ({precio}) => {
+        total += precio;
+    })
+
+    return [total, total * 0.16];
+}
+
+const articulos = [telefono, tablet];
+
+const [ total, iva ] = calculaIVA( articulos );
+
+console.log('Total: ', total);
+console.log('IVA:', iva);
